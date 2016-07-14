d35b40ba953a5a864cebe8e2c1886a8e515b0faa
charltonaustin <charlton.austin@gmail.com>
2016-07-14 15:26:19 +0300

    Adding in words.

--- telegram-problem/src/telegram-problem.js
+++ telegram-problem/src/telegram-problem.js
@@ -1,6 +1,25 @@
 var createWriter = function(w){
   var writer = function(lines){
-     
+    var output = "";
+    var words = lines.split(" ")
+    var currentOutput = words[0];
+    var currentLineLength = words[0].length;
+    for(var i = 1; i< words.length; i++){
+      var word = words[i];
+      if(currentLineLength + word.length + 1 > w){
+        currentOutput[currentOutput.length-1] = "\n";
+        output += currentOutput + "\n";
+        currentOutput = word;
+        currentLineLength = word.length;
+      } else {
+        currentOutput += "." + word;
+        currentLineLength += word.length + 1;
+      }
+    }
+    output+= currentOutput;
+    console.log("output: ")
+    console.log(output);
+    return output;
   }
   return writer;
 }
--- telegram-problem/test/spec/test.js
+++ telegram-problem/test/spec/test.js
@@ -8,4 +8,27 @@
       });
     });
   });
+  
+  describe('writer with w', function () {
+    var length = 10;
+    var defaultInput ="a b c d e f g h"; 
+    var writer = createWriter(length);
+
+    describe('when called', function(){
+      it('should return empty output', function(){
+        expect(writer("")).toBe("");
+      });
+
+      it('should return as many chars as possible', function() {
+        expect(writer(defaultInput).split("\n")[0].length).toBe(9);
+      });
+
+      it('should return as many words as possible', function(){
+        var output = writer("I am a cat what are you?").split("\n");
+        expect(output[0].length).toBe(10);
+        expect(output[1].length).toBe(8);
+        expect(output[2].length).toBe(4);
+      });
+    });
+  });
 })();
