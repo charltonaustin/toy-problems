1ceec4e4d8845de597f75ba6421b656c114f1cf7
charltonaustin <charlton.austin@gmail.com>
2016-07-15 11:52:49 +0300

    Doing a little warm up for the day.

--- euler-problem-1/src/euler-problem-1.js
+++ euler-problem-1/src/euler-problem-1.js
@@ -1,3 +1,18 @@
-var someFunction = function(){
-  return 0;
+var isMultipleOfThree = function(number) {
+  return (number % 3) === 0;
 }
+
+var isMultipleOfFive = function(number) {
+  return (number % 5) === 0;
+}
+
+var eulerProblemOne = function(upperBound){
+  var sum = 0;
+  for(var i = 0; i < upperBound; i++){
+    if(isMultipleOfThree(i) || isMultipleOfFive(i)){
+      sum += i;
+    }
+  }
+  return sum;
+}
+
--- euler-problem-1/test/spec/test.js
+++ euler-problem-1/test/spec/test.js
@@ -1,11 +1,35 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(someFunction()).toBe(1);
-      });
+  describe('isMultipleOfThree', function () {
+    it('should return false', function () {
+      expect(isMultipleOfThree(1)).toBe(false);
+    });
+    
+    it('should return true', function () {
+      expect(isMultipleOfThree(3)).toBe(true);
+    });
+  });
+
+
+  describe('isMultipleOfFive', function () {
+    it('should return false', function () {
+      expect(isMultipleOfFive(1)).toBe(false);
+    });
+    
+    it('should return true', function () {
+      expect(isMultipleOfFive(5)).toBe(true);
+    });
+  });
+
+  describe('eulerProblemOne', function(){
+    it('should work with 10', function(){
+      var actual = eulerProblemOne(10);
+      expect(actual).toBe(23);
+    });
+
+    it('should work with 1000', function(){
+      expect(eulerProblemOne(1000)).toBe(233168);
     });
   });
 })();
