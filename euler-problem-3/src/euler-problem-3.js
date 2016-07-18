5db243fa1a288ce302ba91708de8c29918e27441
charltonaustin <charlton.austin@gmail.com>
2016-07-18 14:13:59 +0300

    First attempt.

--- euler-problem-3/src/euler-problem-3.js
+++ euler-problem-3/src/euler-problem-3.js
@@ -1,3 +1,29 @@
-var someFunction = function(){
-  return 0;
+var findPrimesLessThan = function(number){
+  var primes = [];
+  for (var i = 2; i < number; i++){
+    primes.push(i);
+  }
+  for(var i = 0; i < Math.sqrt(primes.length); i++){
+    for(var j = i + 1; j < primes.length; j++){
+      if(primes[j] % primes[i] === 0){
+        primes.splice(j,1);
+      }
+    }
+  }
+  return primes;
+}
+
+var isPrimeFactor = function(number, prime){
+  return number % prime === 0;
+}
+
+var findPrimeFactors = function(number){
+  var primes = findPrimesLessThan(number);
+  var largestPrimeFactor = primes[0];
+  for(var i = 0; i < primes.length; i++){
+    if(isPrimeFactor(number, primes[i]) && primes[i] > largestPrimeFactor){
+      largestPrimeFactor = primes[i];
+    }
+  }
+  return largestPrimeFactor;
 }
--- euler-problem-3/test/spec/test.js
+++ euler-problem-3/test/spec/test.js
@@ -1,11 +1,28 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(someFunction()).toBe(1);
-      });
+  describe('findPrimesLessThan', function () {
+    it('should find primes less than 3', function () {
+      expect(findPrimesLessThan(3)).toEqual([2]);
     });
+    it('should find primes less than 10', function(){
+      expect(findPrimesLessThan(10)).toEqual([2,3,5,7])
+    });
+  });
+  describe('isPrimeFactor', function(){
+    it('should see if is prime factor', function(){
+      expect(isPrimeFactor(13195, 5)).toBe(true);
+    });
+  });
+
+  describe('largestPrimeFactor', function(){
+    it('should find largest for 13195', function(){
+      expect(findPrimeFactors(13195)).toBe(29);
+    });
+
+    it('should find largest for 600851475143', function(){
+//      expect(findPrimeFactors(600851475143)).toBe(29);
+    });
+
   });
 })();
