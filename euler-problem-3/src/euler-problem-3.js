90b019a2c519a955bc980c8fb2393ea97cdbdcfd
charltonaustin <charlton.austin@gmail.com>
2016-07-18 16:33:51 +0300

    Creating a second attempt that does not error out due to memory usage.

--- euler-problem-3/src/euler-problem-3.js
+++ euler-problem-3/src/euler-problem-3.js
@@ -27,3 +27,47 @@ var findPrimeFactors = function(number){
   }
   return largestPrimeFactor;
 }
+
+var findFermantFactorization = function(a, number){
+  if(number % 2 === 0){
+    return 0;
+  }
+
+  var bSquared = a * a - number;
+  while(Math.sqrt(bSquared) % 1 !== 0 && a < number){
+    a += 1;
+    bSquared = a*a-number;
+  }
+  if(Math.sqrt(bSquared) % 1 !== 0){
+    return false;
+  }
+  return [a, Math.sqrt(bSquared)];
+}
+
+var isPrimeNumber = function(number){
+  for(var i = 2; i < number; i++){
+    if(number % i === 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+var findPrimeNumber = function(number){
+  var factors = findFermantFactorization(Math.ceil(Math.sqrt(number)), number);
+  while(factors){
+    var addFactor = factors[0] + factors[1];
+    var subFactor = factors[0] - factors[1];
+    console.log("addFactor: ", addFactor)
+    console.log("subFactor: ", subFactor)
+    if(isPrimeNumber(addFactor)){
+      return addFactor;
+    }
+    if(isPrimeNumber(subFactor)){
+      return subFactor;
+    }
+    factors = findFermantFactorization(factors[0]+1, number);
+  }
+
+  return primes;
+}
--- euler-problem-3/test/spec/test.js
+++ euler-problem-3/test/spec/test.js
@@ -19,10 +19,40 @@
     it('should find largest for 13195', function(){
       expect(findPrimeFactors(13195)).toBe(29);
     });
-
-    it('should find largest for 600851475143', function(){
-//      expect(findPrimeFactors(600851475143)).toBe(29);
+    it('should find largest for 61', function(){
+      expect(findPrimeNumber(61)).toBe(61);
+    });
+    it('should find largest for 5001', function(){
+      expect(findPrimeNumber(5001)).toBe(1667);
     });
 
+    it('should find largest for 600851475143', function(){
+     expect(findPrimeNumber(600851475143)).toBe(6857);
+    });
+
+  });
+
+  describe('findFermantFactorization', function(){
+    it('should find fermant factorizatio for 13195', function(){
+      expect(findFermantFactorization(Math.ceil(Math.sqrt(13195)), 13195)).toEqual([118, 27]);
+    });
+    it('should return false if a is too large', function(){
+      expect(findFermantFactorization(13195, 13195)).toEqual(false);
+    });
+  });
+
+  describe('isPrimeNumber', function(){
+    it('should return false', function(){
+      expect(isPrimeNumber(4)).toBe(false);
+    });
+    it('should return true', function(){
+      expect(isPrimeNumber(5)).toBe(true);
+    });
+  });
+
+  describe('findPrimeNumber',function(){
+    it('should return a prime number', function(){
+      expect(findPrimeNumber(13195)).toBe(29);
+    })
   });
 })();
