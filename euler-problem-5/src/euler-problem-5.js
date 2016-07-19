a5f416a3a26623cb52f7a1fe5875e67d8a2dc449
charltonaustin <charlton.austin@gmail.com>
2016-07-19 09:35:48 +0300

    Adding in a test for isDivisibleByRange

--- euler-problem-5/src/euler-problem-5.js
+++ euler-problem-5/src/euler-problem-5.js
@@ -3,3 +3,22 @@ var isDivisibleCreator = function(divisibleBy){
     return number % divisibleBy === 0;
   };
 }
+
+var isDivisibleByRangeCreator = function(range){
+  var checkers = [];
+  for(var i = 1; i < range + 1; i++){
+    checkers.push(isDivisibleCreator(i));
+  }
+  return function(number){
+    for(var i = 0; i < checkers.length; i++){
+      if(!checkers[i](number)){
+        return false;
+      }
+    }
+    return true;
+  };
+}
+
+var smallestDivisbleBy = function(){
+  return 0;
+}
--- euler-problem-5/test/spec/test.js
+++ euler-problem-5/test/spec/test.js
@@ -16,4 +16,27 @@
       });
     });
   });
+
+  describe('isDivisibleByRangeCreator',function(){
+    var isDivisibleByRange123 = isDivisibleByRangeCreator(3);
+    it('should create a function',function(){
+      expect(isDivisibleByRange123).toEqual(jasmine.any(Function));
+    });
+
+    describe('should create isDivisibleByRange123 and', function(){
+      it('should return true', function(){
+        expect(isDivisibleByRange123(9)).toBe(false);
+      });
+
+      it('should return false', function(){
+        expect(isDivisibleByRange123(6)).toBe(true);
+      });
+    });
+  });
+
+  describe('smallestDivisbleBy',function(){
+    it('should work for number 1 through 10',function(){
+      expect(smallestDivisbleBy(10)).toBe(2520);
+    });
+  });
 })();
