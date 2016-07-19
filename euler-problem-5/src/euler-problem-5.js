607042b0cc41b17007f4a06352ed6b91080bab12
charltonaustin <charlton.austin@gmail.com>
2016-07-19 09:14:42 +0300

    Testing that we create higher order functions correctly

--- euler-problem-5/src/euler-problem-5.js
+++ euler-problem-5/src/euler-problem-5.js
@@ -1,3 +1,5 @@
-var someFunction = function(){
-  return 0;
+var isDivisibleCreator = function(divisibleBy){
+  return  function(number){
+    return number % divisibleBy === 0;
+  };
 }
--- euler-problem-5/test/spec/test.js
+++ euler-problem-5/test/spec/test.js
@@ -1,10 +1,19 @@
 (function () {
   'use strict';
 
-  describe('someFunction', function () {
-    it('should return 1', function () {
-      expect(someFunction()).toBe(1);
+  describe('isDivisibleCreator', function () {
+    var isEven = isDivisibleCreator(2);
+    it('should return a function', function () {
+      expect(isEven).toEqual(jasmine.any(Function));
+    });
+
+    describe('should create an even function', function(){
+      it('should return true', function(){
+        expect(isEven(2)).toBe(true);
+      });
+      it('should return false', function(){
+        expect(isEven(3)).toBe(false);
+      });
     });
   });
- 
 })();
