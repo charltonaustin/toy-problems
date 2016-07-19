7441a8a7a95bd361b8d1ca464add2c293a6fc5a3
charltonaustin <charlton.austin@gmail.com>
2016-07-19 12:17:42 +0300

    Adding in the toy problem for tomorrow.

---
+++ euler-problem-6/test/spec/test.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+
+  describe('someFunction', function () {
+    it('should return 1', function () {
+      expect(someFunction()).toBe(1);
+    });
+  });
+ 
+})();
