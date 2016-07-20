ab5cecf9713d32463b03af0bd7dd6ecc342ea13e
charltonaustin <charlton.austin@gmail.com>
2016-07-20 05:42:59 +0300

    Adding in an easy solution today.

--- euler-problem-6/src/euler-problem-6.js
+++ euler-problem-6/src/euler-problem-6.js
@@ -1,3 +1,11 @@
-var someFunction = function(){
-  return 0;
+var findDifferenceOfSquares = function(number){
+  var differences = [];
+  var sum = 0;
+  var sumOfSquares = 0;
+  for(var i = 1; i < number + 1; i++) {
+    sum += i;
+    sumOfSquares += i * i;
+    differences.push(sum*sum - sumOfSquares);
+  }
+  return differences;
 }
--- euler-problem-6/test/spec/test.js
+++ euler-problem-6/test/spec/test.js
@@ -1,9 +1,21 @@
 (function () {
   'use strict';
 
-  describe('someFunction', function () {
-    it('should return 1', function () {
-      expect(someFunction()).toBe(1);
+  describe('findDifferenceOfSquares', function () {
+    it('should work for 1', function () {
+      expect(findDifferenceOfSquares(1)).toEqual([0]);
+    });
+
+    it('should work for 2', function () {
+      expect(findDifferenceOfSquares(2)).toEqual([0, 4]);
+    });
+
+    it('should work for 10', function(){
+      expect(findDifferenceOfSquares(10)[9]).toEqual(2640);
+    });
+
+    it('should work for 100', function(){
+      expect(findDifferenceOfSquares(100)[99]).toEqual(25164150);
     });
   });
  
