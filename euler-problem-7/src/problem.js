ff9a1ce7659bcf71d12f353f1ee804763c9b9a69
charltonaustin <charlton.austin@gmail.com>
2016-07-21 14:49:35 +0300

    Adding in solution to today's toy problem

--- euler-problem-7/src/problem.js
+++ euler-problem-7/src/problem.js
@@ -1,6 +1,40 @@
-var someFunction = function(){
-  return 0;
+var findPrimeN = function(number){
+  var primes = [2, 3]
+  var numbers = [];
+  while(primes.length < number){
+    var start = primes[primes.length-1] + 1;
+    var growth = primes[primes.length-1];
+    // Grow the numbers array
+    for(var i = start; i < start + growth; i++){
+      numbers.push(i);
+    }
+
+    // Remove composite numbers
+    for(var i = 0; i < primes.length; i++){
+      for(var j = 0; j < numbers.length; j++){
+        if(numbers[j] % primes[i] === 0){
+          numbers.splice(j,1);
+        }
+      }
+    }
+
+
+    for(var i = 0; i < numbers.length; i++){
+      for(var j = 1; j < numbers.length; j++){
+        if(numbers[j] % number[i] === 0){
+          number.splice(j,1);
+        }
+      }
+    }
+
+    // move a prime from numbers to primes
+    for(var i = 0; i < numbers.length; i++){
+      primes.push(numbers[i]);
+    }
+    numbers = [];
+  }
+  return primes[number - 1];
 }
 
 
-module.exports = someFunction;
+module.exports = findPrimeN;
--- euler-problem-7/test/mySpec.js
+++ euler-problem-7/test/mySpec.js
@@ -1,10 +1,47 @@
 (function () {
   'use strict';
-  var someFunction = require('../src/problem.js');
-  describe('someFunction', function () {
-    it('should return 1', function () {
-      expect(someFunction()).toBe(1);
+  var findPrimeN = require('../src/problem.js');
+  describe('findPrimeN', function () {
+    it('should return 2 for first prime', function () {
+      expect(findPrimeN(1)).toBe(2);
     });
+
+    it('should return 3 for the second prime', function () {
+      expect(findPrimeN(2)).toBe(3);
+    });
+
+    it('should return 5 for the third prime', function () {
+      expect(findPrimeN(3)).toBe(5);
+    });
+
+    it('should return 7 for the fourth prime', function () {
+      expect(findPrimeN(4)).toBe(7);
+    });
+
+    it('should return 11 for the fifth prime', function () {
+      expect(findPrimeN(5)).toBe(11);
+    });
+
+    it('should return 13 for the sixth prime', function () {
+      expect(findPrimeN(6)).toBe(13);
+    });
+
+    it('should return 73 for the 21st prime', function () {
+      expect(findPrimeN(21)).toBe(73);
+    });
+
+    it('should return 79 for the 22nd prime', function () {
+      expect(findPrimeN(22)).toBe(79);
+    });
+
+    it('should return 541 for the 100th prime', function () {
+      expect(findPrimeN(100)).toBe(541);
+    });
+
+    it('should return ??? for the 1001th prime', function () {
+      expect(findPrimeN(1001)).toBe(7927);
+    });
+
   });
  
 })();
