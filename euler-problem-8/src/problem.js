26f180ca309392271696afdc3a13e1ce8a2ecd5e
charltonaustin <charlton.austin@gmail.com>
2016-07-22 23:07:45 +0300

    Adding in a solution to today's toy problem.

--- euler-problem-8/src/problem.js
+++ euler-problem-8/src/problem.js
@@ -1,6 +1,17 @@
-var someFunction = function(){
-  return 0;
+var findLargestMultiple = function(string, length){
+  var largestMultiple = 0;
+  for(var i = 0; i < string.length-length; i++){
+    var currentMultiple = 1;
+    for(var j = i; j < i+length; j++){
+      console.log(Number(string[j]));
+      currentMultiple *= Number(string[j]);
+    }
+    if(currentMultiple > largestMultiple){
+      largestMultiple = currentMultiple;
+    }
+  }
+  return largestMultiple;
 }
 
 
-module.exports = someFunction;
+module.exports = findLargestMultiple;
