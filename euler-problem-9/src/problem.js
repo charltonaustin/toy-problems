c941e2b740ffbc4617faad52ae5b6177346795d2
charltonaustin <charlton.austin@gmail.com>
2016-07-23 08:35:36 +0300

    Removing some comments

--- euler-problem-9/src/problem.js
+++ euler-problem-9/src/problem.js
@@ -1,17 +1,3 @@
-/*
- A Pythagorean triplet is a set of 
-three natural numbers, a < b < c, 
-for which,
-
-a^2 + b^2 = c^2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
-
-There exists exactly one Pythagorean 
-triplet for which a + b + c = 1000.
-Find the product abc.
-
- */
-
 var findPythagoreanTriplet = function(){
   for(var a = 1; a < 1000; a++){
     for(var b = a+1; b < 1000; b++){
