760dbf36ece911695bf1bccdc76ec9108705a8bb
charltonaustin <charlton.austin@gmail.com>
2016-07-24 10:01:07 +0300

    Addin gin the solution to today's problem.

--- euler-problem-11/src/problem.js
+++ euler-problem-11/src/problem.js
@@ -1,6 +1,56 @@
-var someFunction = function(){
-  return 0;
-}
 
+var largestProductOfFour = function(matrix) {
+  var largestMultiple = 0;
+  var length = 4;
+  for(var i = 0; i < matrix.length; i++){
+    for(var j = 0; j < matrix[0].length; j++){
 
-module.exports = someFunction;
+      // forward
+      var currentMultiple = 1;
+      for(var k = 0; k < length; k++){
+        if(j+k < matrix[0].length){
+          currentMultiple *= matrix[i][j+k];
+        }
+      }
+      if(currentMultiple > largestMultiple){
+        largestMultiple = currentMultiple;
+      }
+
+      // downward
+      currentMultiple = 1;
+      for(var k = 0; k < length; k++){
+        if(i+k < matrix.length){
+          currentMultiple *= matrix[i+k][j];
+        }
+      }
+      if(currentMultiple > largestMultiple){
+        largestMultple = currentMultiple;
+      }
+
+      // diagonal-up-back
+      currentMultiple = 1;
+      for(var k = 0; k < length; k++){
+        if(i-k > 0 && j-k > 0){
+          currentMultiple *= matrix[i-k][j-k];
+        }
+      }
+      if(currentMultiple > largestMultiple){
+        clargestMultple = currentMultiple;
+      }
+
+      // diagonal-up-forward
+      currentMultiple = 1;
+      for(var k = 0; k < length; k++){
+        if(i-k > 0 && j+k < matrix[0].length){
+          currentMultiple *= matrix[i-k][j+k];
+        }
+      }
+      if(currentMultiple > largestMultiple){
+        largestMultple = currentMultiple;
+      }
+    }
+  }
+  return largestMultple;
+};
+
+module.exports = largestProductOfFour;
