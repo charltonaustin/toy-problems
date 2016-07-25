cad6401412419fcc51cbf23344415d89af49758b
charltonaustin <charlton.austin@gmail.com>
2016-07-25 16:33:51 +0300

    Putting in a suboptimal solution.

--- euler-problem-10/src/problem.js
+++ euler-problem-10/src/problem.js
@@ -1,6 +1,28 @@
-var someFunction = function(){
-  return 0;
+var primesLessThan = (number) => {
+  var primes = [2];
+  for(var i = 3; i < number; i+=2){
+    primes.push(i);
+  }
+  console.log("finished generating all the number");
+  for(var i = 0; i < primes.length; i++){
+    console.log("removing: ", primes.length);
+    for(var j = i + 1; j < primes.length; j++){
+      if(primes[j] % primes[i] === 0){
+        primes.splice(j,1);
+      }
+    }
+  }
+  return primes;
 }
 
+var sumOfPrimesLessThan = (number) =>{
+  var primes = primesLessThan(number);
+  console.log(primes);
+  var total = 0;
+  for(var i = 0; i < primes.length; i++){
+    total+= primes[i];
+  }
+  return total;
+}
 
-module.exports = someFunction;
+module.exports = {sumOfPrimesLessThan};
--- euler-problem-10/test/mySpec.js
+++ euler-problem-10/test/mySpec.js
@@ -1,9 +1,10 @@
 (function () {
   'use strict';
-  var someFunction = require('../src/problem.js');
-  describe('someFunction', function () {
+  var sumOfPrimesLessThan = require('../src/problem.js').sumOfPrimesLessThan;
+  describe('sumOfPrimesLessThan', function () {
     it('should return 1', function () {
-      expect(someFunction()).toBe(1);
+      expect(sumOfPrimesLessThan(10)).toBe(17);
+      expect(sumOfPrimesLessThan(2000000)).toBe(142913828922);
     });
   });
  
