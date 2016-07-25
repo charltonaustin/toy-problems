ef790c0a789642a2899ecdfde6958acf3b69d04b
charltonaustin <charlton.austin@gmail.com>
2016-07-25 09:01:07 +0300

    Writing my first couple of tests.

--- euler-problem-12/test/mySpec.js
+++ euler-problem-12/test/mySpec.js
@@ -1,10 +1,23 @@
-(function () {
+(() => {
   'use strict';
-  var someFunction = require('../src/problem.js');
-  describe('someFunction', function () {
-    it('should return 1', function () {
-      expect(someFunction()).toBe(1);
+  var highlyDivisible = require('../src/problem.js').highlyDivisible;
+  var findPrimeNumbersLessThan = require('../src/problem.js').findPrimeNumbersLessThan;
+  var getDivisors = require('../src/problem.js').getDivisors;
+  describe('highlyDivisible',  () => {
+    it('should find lowest number with over five divisors',  () => {
+      expect(highlyDivisible(5)).toBe(29);
     });
   });
  
+  describe('findPrimeNumbersLessThan', () => {
+    it('should find the prime numbers less than 10', () => {
+      expect(findPrimeNumbersLessThan(10)).toEqual([2,3,5,7])
+    });
+  });
+
+ describe('getDivisors', () => {
+    it('should find divisors in array', () => {
+      expect(getDivisors(10,[2,5,6,8])).toEqual([2,5])
+    });
+  });
 })();
