95c29192e332842a8e0a3e3900ac18c09fa823b7
charltonaustin <charlton.austin@gmail.com>
2016-07-29 14:17:50 +0300

    Fixing some spacing issues.

--- euler-problem-16/test/mySpec.js
+++ euler-problem-16/test/mySpec.js
@@ -2,15 +2,14 @@
   'use strict';
   var sumPowerOfTwo = require('../src/problem.js').sumPowerOfTwo;
   var findPowerOfTwo = require('../src/problem.js').findPowerOfTwo;
+
   describe('sumPowerOfTwo', function () {
     it('should return 26', function () {
       expect(sumPowerOfTwo(15)).toBe("26");
     });
-
     it('should return 5', function () {
       expect(sumPowerOfTwo(5)).toBe("5");
     });
-    
     it('should return 1366', function () {
       expect(sumPowerOfTwo(1000)).toBe("1366");
     });
