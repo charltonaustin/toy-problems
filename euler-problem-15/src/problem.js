3941b975e81a7dc035497c621c140beefbb5fb45
charltonaustin <charlton.austin@gmail.com>
2016-07-30 12:09:04 +0300

    Fixing the solution to problem 15 because my first attempt was  seriously to slow.

--- euler-problem-15/src/problem.js
+++ euler-problem-15/src/problem.js
@@ -1,64 +1,76 @@
 /*
 Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
-
 How many such routes are there through a 20×20 grid?
 */
 
+var createNode = (i, j, value) => {
+  return {i: i, j: j, value:value};
+}
+
 var createGraph = (numberOfBlocks) =>{
-  var edges = [];
+  var edges = {};
   var length = numberOfBlocks + 1
   for(var i = 0; i < length; i++){
     for(var j = 0; j < length; j++){
-      var currentEdges = [];
+      var currentEdges = {};
       if(j+1 < length){
-        currentEdges.push(length*i + (j+1));
+        currentEdges['right'] = createNode(i, j+1, length*i + (j+1));
       }   
       if(i+1 < length){
-        currentEdges.push(length*(i+1) + j);
+        currentEdges['down'] = createNode(i+1, j, length*(i+1) + j);
       }
-      edges.push(currentEdges);
+      currentEdges['value'] = length*i + j;
+      currentEdges['j'] = j;
+      currentEdges['i'] = i;
+      edges[length*i + j] = currentEdges;
     }
   }
   return edges;
 }
 
-var nodeString = (node) => {
-  var str = "";
-  for(var i=0; i < node.length; i++){
-    str+=node[i]+":";
+var toTheLeftOf = (node,endNode) => {
+  return  node.j < endNode.j
+}
+
+var isAbove = (node, endNode) => {
+  return node.i < endNode.i
+}
+
+var countPaths = (node, endNode, graph, numberOfPaths) => {
+  var numberOfPaths = 0;
+  var toVisit = [node];
+  var lastPlaceTurnedRight = [];
+  while(toVisit.length > 0){
+    node = toVisit.pop();
+    if(node.value === endNode.value){
+      numberOfPaths++;
+      var lastRight = lastPlaceTurnedRight.pop();
+      if(lastRight){
+        toVisit.push(graph[lastRight['down']['value']]);
+      }
+    }else if(isAbove(node, endNode) && toTheLeftOf(node,endNode)){
+      lastPlaceTurnedRight.push(node);
+      toVisit.push(graph[node['right']['value']]);
+    }else if(toTheLeftOf(node, endNode)){
+      toVisit.push(graph[node['right']['value']]);
+    }else if(isAbove(node, endNode)){
+      toVisit.push(graph[node['down']['value']]);
+    }
   }
-  return str;
+  return numberOfPaths;
 }
 
 var findNumberOfRoutes = (numberOfBlocks) => {
   var graph = createGraph(numberOfBlocks);
-  var numberOfPaths = 0;
-  var alreadyCounted = {};
-  var countPaths = (node) => { 
-    var toVisit = [];
-    toVisit.push(node);
-    var lastPlaceTurnedRight = [];
-    while(toVisit.length > 0){
-      node = toVisit.pop();
-      if(node.length === 0 || alreadyCounted[nodeString(node)]){
-        numberOfPaths++;
-        var lastRight = lastPlaceTurnedRight.pop();
-        if(lastRight){
-          toVisit.push(graph[lastRight[1]]);
-        }
-      }else if(node.length === 2){
-        lastPlaceTurnedRight.push(node);
-        toVisit.push(graph[node[0]]);
-      }else if (node.length === 1){
-        toVisit.push(graph[node[0]]);
-        alreadyCounted[nodeString(node)] = true;
-      }
-    }
+  var numberOfPaths = [];
+  for(var i = 1; i < numberOfBlocks + 2; i++){
+    numberOfPaths.push(countPaths(graph[0], graph[numberOfBlocks * i], graph));
   }
-  countPaths(graph[0])
-  return numberOfPaths;
+  var total = 0;
+  for(var i = 0; i < numberOfPaths.length; i++){
+    total += numberOfPaths[i] * numberOfPaths[i];
+  }
+  return total;
 }
 
-
-module.exports = {findNumberOfRoutes,
-                  createGraph};
+module.exports = findNumberOfRoutes;
--- euler-problem-15/test/mySpec.js
+++ euler-problem-15/test/mySpec.js
@@ -1,14 +1,6 @@
 (function () {
   'use strict';
-  var findNumberOfRoutes = require('../src/problem.js').findNumberOfRoutes;
-  var createGraph = require('../src/problem.js').createGraph;
-  describe('createGraph', function () {
-    it('should create the correct graph', function () {
-      expect(createGraph(2)[0]).toEqual([1,3]);
-      expect(createGraph(2)[3]).toEqual([4,6]);
-      expect(createGraph(2)[8]).toEqual([]);
-    });
-  });
+  var findNumberOfRoutes = require('../src/problem.js');
   describe('findNumberOfRoutes', function () {
     it('should find the correct number for 2', function () {
       expect(findNumberOfRoutes(2)).toEqual(6);
@@ -21,9 +13,7 @@
 
       expect(findNumberOfRoutes(6)).toEqual(924);
 
+      expect(findNumberOfRoutes(20)).toEqual(137846528820);
     });
   });
-
-  
- 
 })();
