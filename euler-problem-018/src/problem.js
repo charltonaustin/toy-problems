7b31abd4f9d8b72d9a87cc95ac29a198f9c8663d
charltonaustin <charlton.austin@gmail.com>
2016-08-02 17:44:43 +0300

    Fixing euler-problem-018

--- euler-problem-018/src/problem.js
+++ euler-problem-018/src/problem.js
@@ -1,16 +1,10 @@
-
-
 /*By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
-
    3 
   7 4
  2 4 6
 8 5 9 3
-
 That is, 3 + 7 + 4 + 9 = 23.
-
 Find the maximum total from top to bottom of the triangle below:
-
                    75
                   95 64
                 17 47 82
@@ -26,93 +20,28 @@ Find the maximum total from top to bottom of the triangle below:
       91 71 52 38 17 14 91 43 58 50 27 29 48
      63 66 04 68 89 53 67 30 73 16 69 87 40 31
     04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 */
 
-class Tree {
-  constructor(number, id = 0) {
-    this.value = number;
-    this.left = undefined;
-    this.right = undefined;
-    this.visited = false;
-    this.id = id;
-  }
-  insert(number) {
-    if(this.left === undefined){
-      this.left = new Tree(number, this.id+1);
-      return this.left;
-    }
-    if(this.right === undefined){
-      this.right = new Tree(number, this.id+2);
-      return this.right;
-    }
-  }
-}
-
-var parseTriangle = (triangle) => {
-  var lines = triangle.trim().split("\n");
-  var ints = [];
-  
-  for(var i = 0; i < lines.length; i++){
-    var nums = lines[i].trim().split(" ");
-    for(var j = 0; j < nums.length; j++){
-      var currentNum = parseInt(nums[j]);
-      ints.push(currentNum);
-    }
-  }
-
-  var root = new Tree(ints[0]);
-  var nodes = [root];
-  for(var i = 0; i*2 < ints.length-2; i++){
-    nodes[i].insert(ints[i*2 + 1]);
-    nodes[i].insert(ints[i*2 + 2]);
-    nodes.push(nodes[i].left);
-    nodes.push(nodes[i].right);
-  }
-  
-  return root;
-}
-
-var depthFirst = (rootNode) => {
-  var toVisit = [rootNode];
-  var visitAfter = [];
-  var totals = []
-  var currentTotal = 0;
-  var totalsAt = {}
-  while(toVisit.length > 0){
-    var node = toVisit.pop();
-    totalsAt[node.id] = currentTotal;
-    currentTotal += node.value;
-    if(node.left && !node.left.visited){
-      node.left.visited = true;
-      toVisit.push(node.left);
-      visitAfter.push(node);
-    }else if(node.right && !node.right.visited){
-      node.right.visited = true;
-      toVisit.push(rootNode.right);
-    }else {
-      var maybeNode = visitAfter.pop();
-      totals.push(currentTotal);
-      if(maybeNode){
-        currentTotal = totalsAt[maybeNode.id];
-        toVisit.push(maybeNode);
-      }
-    }
-  }
-  return totals;
-}
-
 var findMaximumTotal = (triangle) => {
-  var tree = parseTriangle(triangle);
-  var totals = depthFirst(tree); 
-  var max = totals[0] || 0;
-  for(var i = 0; i < totals.length; i++){
-    if(max < totals[i]){
-      max = totals[i];
-    }
+  var lines = triangle.trim().split("\n");
+  var totals = [];
+  var lineBefore = [];
+  var stringLine = lines[lines.length-1].trim().split(" ");
+  for(var i = 0; i < stringLine.length; i++){
+    lineBefore.push(parseInt(stringLine[i]));
   }
-  return max;
+  
+  for(var i = lines.length-2; i >= 0; i--){
+    var currentLine = lines[i].trim().split(" ");
+    var temp = [];
+    for(var j = 0; j < currentLine.length; j++){
+      var currentNum = parseInt(currentLine[j]);
+      temp.push(Math.max(currentNum+lineBefore[j], currentNum+lineBefore[j+1]));
+    }
+    lineBefore = temp;
+  }
+  return lineBefore[0] || 0;;
 }
 
-module.exports = {findMaximumTotal, parseTriangle, depthFirst};
+module.exports = {findMaximumTotal};
--- euler-problem-018/test/mySpec.js
+++ euler-problem-018/test/mySpec.js
@@ -2,32 +2,22 @@
   'use strict';
   var problem = require('../src/problem.js')
   var findMaximumTotal = problem.findMaximumTotal;
-  var parseTriangle = problem.parseTriangle;
-  var depthFirst = problem.depthFirst;
-
-  describe('parseTriangle', function () {
-    it('should return a tree', function () {
-      expect(parseTriangle("1\n2 3").value).toEqual(1);
-      expect(parseTriangle("1\n2 3").left.value).toEqual(2);
-      expect(parseTriangle("1\n2 3").right.value).toEqual(3);
+  var bigTriangle = "75\n95 64\n17 47 82\n18 35 87 10\n20 4 82 47 65\n19 1 23 75 3 34\n88 2 77 73 7 63 67\n99 65 4 28 6 16 70 92\n41 41 26 56 83 40 80 70 33\n41 48 72 33 47 32 37 16 94 29\n53 71 44 65 25 43 91 52 97 51 14\n70 11 33 28 77 73 17 78 39 68 17 57\n91 71 52 38 17 14 91 43 58 50 27 29 48\n63 66 4 68 89 53 67 30 73 16 69 87 40 31\n4 62 98 27 23 9 70 98 73 93 38 53 60 4 23\n";
+  describe('findMaximumTotal',()=>{
+    it('should find the max of a single element triangle', ()=>{
+      expect(findMaximumTotal("3")).toBe(3);
     });
-    it('should a larger tree', function () {
-      expect(parseTriangle("3 \n 7 4 \n  2 4 6\n   8 5 9 3").left.left.value).toEqual(2);
-      expect(parseTriangle("3 \n 7 4 \n  2 4 6\n   8 5 9 3").right.right.value).toEqual(6);
+    it('should find the max of a three element triangle', ()=>{
+      expect(findMaximumTotal("3\n 7 4\n")).toBe(10);
     });
-  });
-
-  describe('depthFirst', function () {
-    it('should an array for triangle with four levels', function () {
-      expect(depthFirst(parseTriangle("1\n2 3"))).toEqual([3, 4]);
+    it('should find the max of a three row triangle', ()=>{
+      expect(findMaximumTotal("3\n 7 4\n 2 4 6\n")).toBe(14);
     });
-  });
-
-  describe('findMaximumTotal', function () {
-    it('should return the max', function () {
-      expect(findMaximumTotal("1\n2 3")).toBe(4);
-      expect(findMaximumTotal("3 \n 7 4 \n  2 4 6\n   8 5 9 3")).toBe(23);
-      expect(findMaximumTotal("75\n95 64\n17 47 82\n18 35 87 10\n20 04 82 47 65\n19 01 23 75 03 34\n88 02 77 73 07 63 67\n99 65 04 28 06 16 70 92\n41 41 26 56 83 40 80 70 33\n41 48 72 33 47 32 37 16 94 29\n53 71 44 65 25 43 91 52 97 51 14\n70 11 33 28 77 73 17 78 39 68 17 57\n91 71 52 38 17 14 91 43 58 50 27 29 48\n63 66 04 68 89 53 67 30 73 16 69 87 40 31\n04 62 98 27 23 09 70 98 73 93 38 53 60 04 23\n")).toBe(1074);
+    it('should find the max of a four row triangle', ()=>{
+      expect(findMaximumTotal("3\n 7 4\n 2 4 6\n8 5 9 3")).toBe(23);
+    });
+    it('should find the max of a four row triangle', ()=>{
+      expect(findMaximumTotal(bigTriangle)).toBe(1074);
     });
   });
 })();
