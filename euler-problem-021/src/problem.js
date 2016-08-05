b70bc5b409d368cc51eb8af5190cfbd6974dcd0c
charltonaustin <charlton.austin@gmail.com>
2016-08-05 13:29:57 +0300

    Adding in the solution to yesterday's toy problem

--- euler-problem-021/src/problem.js
+++ euler-problem-021/src/problem.js
@@ -1,5 +1,64 @@
-var someFunction = () => {
-  return 0;
+/*
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+*/
+
+var properDivisors = (number) => {
+  if(number === 1){
+    return [];
+  }
+  var divisors = [1];
+  for(var i = 2; i <= Math.sqrt(number); i++){
+    if(number % i === 0){
+      var otherDivisor = number / i
+      if(i === otherDivisor){
+        divisors.push(i);
+      }else{
+        divisors.push(i, otherDivisor);
+      }
+    }
+  }
+  return divisors;
 }
 
-module.exports = {someFunction};
+var findSumOfNumbers = (numbers) => {
+  var total = 0;
+  for(var i = 0; i < numbers.length; i++){
+    total+=numbers[i];
+  }
+  return total;
+}
+
+var hasAmicableNumberLessThan = (limit, number) => {
+  debugger;
+  var divisors = properDivisors(number);
+  var sum = findSumOfNumbers(divisors);
+  if(sum < limit && sum != number){
+    var sumDivisors = properDivisors(sum);
+    var sumDivisorsSum = findSumOfNumbers(sumDivisors);
+    if(sumDivisorsSum === number){
+      return sum;
+    }
+  }
+  return false;
+}
+
+var findSumOfAmicableNumbers = (limit) => {
+  var total = 0;
+  var amicableNumbers = {};
+  for(var i = 1; i < limit+1; i++){
+    var maybeAmicable = hasAmicableNumberLessThan(limit, i);
+    if(maybeAmicable && !amicableNumbers[maybeAmicable]){
+      amicableNumbers[maybeAmicable] = true;
+      amicableNumbers[i] = true;
+      total += maybeAmicable + i;
+    }
+  }
+  return total;
+}
+
+module.exports = {properDivisors, findSumOfAmicableNumbers, hasAmicableNumberLessThan};
--- euler-problem-021/test/mySpec.js
+++ euler-problem-021/test/mySpec.js
@@ -1,10 +1,53 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var properDivisors = problem.properDivisors;
+  var findSumOfAmicableNumbers = problem.findSumOfAmicableNumbers;
+  var hasAmicableNumberLessThan = problem.hasAmicableNumberLessThan;
+
+  describe('properDivisors',  () => {
+    describe('of 1', ()=>{
+      it('should return 1',  () => {
+        expect(properDivisors(1)).toEqual([]);
+      });
+    })
+    describe('of 2', ()=>{
+      it('should return 2',  () => {
+        expect(properDivisors(2)).toEqual([1]);
+      });
+    })
+    describe('of 10', ()=>{
+      it('should return 1 2 5',  () => {
+        expect(properDivisors(10)).toEqual([1, 2, 5]);
+      });
+    })
+    describe('of 25', ()=>{
+      it('should return 1 5',  () => {
+        expect(properDivisors(25)).toEqual([1, 5]);
+      });
+    })
+    describe('of 220', ()=>{
+      it('should return 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110',  () => {
+        expect(properDivisors(220)).toEqual([1, 2, 110, 4, 55, 5, 44, 10, 22, 11, 20]);
+      });
+    })
+  });
+
+  describe('hasAmicableNumberLessThan',()=>{
+    it('should find 284 for 220',()=>{
+      expect(hasAmicableNumberLessThan(1000, 220)).toBe(284);
+    });
+    it('should find nothing for 6',()=>{
+      expect(hasAmicableNumberLessThan(1000, 6)).toBe(false);
+    });
+    it('should find nothing for 8',()=>{
+      expect(hasAmicableNumberLessThan(1000, 8)).toBe(false);
+    });
+  });
+
+  describe('findSumOfAmicableNumbers',()=>{
+    it('should return ??? for 1000',()=>{
+      expect(findSumOfAmicableNumbers(10000)).toBe(31626);
     });
   });
 })();
