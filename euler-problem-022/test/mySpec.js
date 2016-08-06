3c6b086e2d1001308b48e652e8e502bc2a990c82
charltonaustin <charlton.austin@gmail.com>
2016-08-06 15:44:13 +0300

    Adding in the solution to yesterday's problem

--- euler-problem-022/test/mySpec.js
+++ euler-problem-022/test/mySpec.js
@@ -1,10 +1,84 @@
 (function () {
   'use strict';
+  var fs = require('fs');
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var readFile = problem.readFile;
+  var totalNameScores = problem.totalNameScores;
+  var getScoreOfWord = problem.getScoreOfWord;
+  var sortContents = problem.sortContents;
+  var contents = fs.readFileSync("./names.txt").toString().split(",");
+  var verifyContents = fs.readFileSync("./names.txt").toString().split(",").sort();
+
+  var readFileStub = ()=>{
+    return "CHARLIE,BRIAN,THOMAS"
+  };
+  var sortContentsStub = () => {
+    return ["BRIAN","CHARLIE"];
+  }
+
+  describe('readFile',  () => {
+    it('should return an array of names',  () => {
+      expect(readFile(readFileStub)).toEqual(['CHARLIE', 'BRIAN', 'THOMAS']);
     });
   });
+
+  describe('sortContents',  () => {
+    it('should return an ordered array',  () => {
+      expect(sortContents([9,8,7])).toEqual([7,8,9]);
+    });
+    it('should return an ordered array',  () => {
+      expect(sortContents(["Z", "B", "A"])).toEqual(["A","B","Z"]);
+    });
+    it('should return an ordered array',  () => {
+      expect(sortContents(['CHARLIE', 'BRIAN', 'THOMAS'])).toEqual(['BRIAN', 'CHARLIE', 'THOMAS']);
+    });
+    it('should return an ordered array',  () => {
+      expect(sortContents(['AAAB', 'AAAA'])).toEqual(['AAAA', 'AAAB']);
+    });
+    it('should return an ordered array',  () => {
+      expect(sortContents(['AAAA', 'A'])).toEqual(['A', 'AAAA']);
+    });
+    it('should return an ordered array',  () => {
+      var actual = sortContents(contents);
+      expect(actual[0]).toEqual(verifyContents[0]);
+
+    });
+  });
+
+  describe('getScoreOfWord',  () => {
+    describe('A', () => {
+      it('should return the correct score',  () => {
+        expect(getScoreOfWord("A")).toEqual(65);
+      });
+    });
+    describe('AAAA', () => {
+      it('should return the correct score',  () => {
+        expect(getScoreOfWord("AAAA")).toEqual(65 * 4);
+      });
+    });
+    describe('BRIAN', () => {
+      it('should return the correct score',  () => {
+        expect(getScoreOfWord("BRIAN")).toEqual(364);
+      });
+    });
+    describe('CHARLIE', () => {
+      it('should return the correct score',  () => {
+        expect(getScoreOfWord("CHARLIE")).toEqual(504);
+      });
+    });
+  });
+
+  describe('totalNameScores',  () => {
+    describe('for stubbed content', ()=>{
+      it('should return a total',  () => {
+        expect(totalNameScores(sortContentsStub)).toBe(1372);
+      });
+    });
+    describe('for file content', ()=>{
+      it('should return a total of ????',  () => {
+        expect(totalNameScores()).toBe(6837803474);
+      });
+    });
+  });
+
 })();
