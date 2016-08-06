d812cf0c6cc89571d912a1c2194097f9098d5643
charltonaustin <charlton.austin@gmail.com>
2016-08-06 18:10:07 +0300

    Adding in the solution to today's toy problem

--- euler-problem-023/src/problem.js
+++ euler-problem-023/src/problem.js
@@ -1,5 +1,110 @@
-var someFunction = () => {
-  return 0;
+/*
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+*/
+
+var isAbundantNumber = (number) => {
+  if(number === 1){
+    return false;
+  }
+  var total = 1;
+  for(var i = 2; i <= Math.sqrt(number); i++){
+    if(number % i === 0){
+      var otherDivisor = number / i
+      if(i === otherDivisor){
+        total+= otherDivisor;
+      }else{
+        total+= i + otherDivisor;
+      }
+      if(total > number){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-module.exports = {someFunction};
+var findAbundantNumbersLessThan = (limit) => {
+  var abundantNumbers = [12];
+  for(var i = 13; i < limit; i++){
+    if(isAbundantNumber(i)){
+      abundantNumbers.push(i);
+    }
+  }
+  return abundantNumbers;
+}
+
+var allPossibleNumbers = (limit) => {
+  var numbers = [];
+  for(var i = 1; i < limit; i++){
+    numbers.push(i);
+  }
+  return numbers;
+}
+
+var filterOutMultiplesOfAbundantNumbers = (abundantNumbers, possibleNumbers) => {
+  for(var i = 0; i < abundantNumbers.length; i++){
+    var abundantNumber = abundantNumbers[i];
+    for(var j = 1; j * abundantNumber <= possibleNumbers[possibleNumbers.length - 1]; j++){
+      for(var k = 0; k < possibleNumbers.length; k++){
+        if(j * abundantNumber === possibleNumbers[k]){
+          possibleNumbers.splice(k,1);
+        }
+      }
+    }
+  }
+  return possibleNumbers;
+}
+
+var filterOutSumsOfAbundantNumbers = (abundantNumbers, possibleNumbers)=>{
+  for(var i = 0; i < abundantNumbers.length; i++){
+    for(var j = i; j < abundantNumbers.length; j++){
+      var sum = abundantNumbers[i]+abundantNumbers[j];
+      for(var k = 0; k < possibleNumbers.length; k++){
+        if(sum === possibleNumbers[k]){
+          possibleNumbers.splice(k,1);
+        }
+      }
+    }
+  }
+  return possibleNumbers
+}
+
+var sumNumbers = (numbers) => {
+  var total = 0;
+  for(var i = 0; i < numbers.length; i++){
+    total+=numbers[i];
+  }
+  return total;
+}
+
+var removeMultiplesOfAbundant = (abundantNumbers)=>{
+  for(var i = 0; i < abundantNumbers.length; i++){
+    for(var j = i + 1; j < abundantNumbers.length; j++){
+      if(abundantNumbers[j] % abundantNumbers[i] === 0){
+        abundantNumbers.splice(j,1);
+      }
+    }
+  }
+  return abundantNumbers;
+}
+
+var findSum = ()=>{
+  console.log("starting");
+  var abundantNumbers = findAbundantNumbersLessThan(28123);
+  console.log("found abundantNumbers, count is: ", abundantNumbers.length);
+  var possibleNumbers = allPossibleNumbers(28123);
+  console.log("found possibleNumbers");
+  var filteredSums = filterOutSumsOfAbundantNumbers(abundantNumbers, possibleNumbers);
+  console.log("found filteredSums");
+  console.log("filteredSums: ", filteredSums);
+  return sumNumbers(filteredSums);
+}
+
+module.exports = {findSum,
+                  sumNumbers,
+                  filterOutSumsOfAbundantNumbers,
+                  filterOutMultiplesOfAbundantNumbers,
+                  allPossibleNumbers,
+                  findAbundantNumbersLessThan,
+                  isAbundantNumber
+                  };
--- euler-problem-023/test/mySpec.js
+++ euler-problem-023/test/mySpec.js
@@ -1,10 +1,97 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var isAbundantNumber = problem.isAbundantNumber;
+  var findAbundantNumbersLessThan = problem.findAbundantNumbersLessThan;
+  var allPossibleNumbers = problem.allPossibleNumbers;
+  var filterOutMultiplesOfAbundantNumbers = problem.filterOutMultiplesOfAbundantNumbers;
+  var filterOutSumsOfAbundantNumbers = problem.filterOutSumsOfAbundantNumbers;
+  var sumNumbers = problem.sumNumbers;
+  var findSum = problem.findSum;
+
+  describe('isAbundantNumber',  () => {
+    it('should return true',  () => {
+      expect(isAbundantNumber(12)).toBe(true);
+      expect(isAbundantNumber(18)).toBe(true);
+      expect(isAbundantNumber(20)).toBe(true);
+      expect(isAbundantNumber(24)).toBe(true);
+      expect(isAbundantNumber(30)).toBe(true);
+      expect(isAbundantNumber(36)).toBe(true);
+      expect(isAbundantNumber(40)).toBe(true);
+      expect(isAbundantNumber(42)).toBe(true);
+      expect(isAbundantNumber(48)).toBe(true);
+    });
+    it('should return false',  () => {
+      expect(isAbundantNumber(2)).toBe(false);
+      expect(isAbundantNumber(19)).toBe(false);
+      expect(isAbundantNumber(21)).toBe(false);
+      expect(isAbundantNumber(25)).toBe(false);
+      expect(isAbundantNumber(32)).toBe(false);
+      expect(isAbundantNumber(37)).toBe(false);
+      expect(isAbundantNumber(41)).toBe(false);
+      expect(isAbundantNumber(43)).toBe(false);
     });
   });
+
+  describe('findAbundantNumbersLessThan',  () => {
+    describe('of 13', ()=>{
+      it('should return [12]',  () => {
+        expect(findAbundantNumbersLessThan(13)).toEqual([12]);
+      });
+    });
+    describe('of 31', ()=>{
+      it('should return [12, 18, 20, 24, 30]',  () => {
+        expect(findAbundantNumbersLessThan(31)).toEqual([12, 18, 20, 24, 30]);
+      });
+    });
+  });
+
+  describe('allPossibleNumbers',  () => {
+    describe('for 3', ()=>{
+      it('should return [1, 2]',  () => {
+        expect(allPossibleNumbers(3)).toEqual([1,2]);
+      });
+    });
+    describe('for 6', ()=>{
+      it('should return [1, 2, 3, 4, 5]',  () => {
+        expect(allPossibleNumbers(6)).toEqual([1,2,3,4,5]);
+      });
+    });
+  });
+
+  describe('filterOutMultiplesOfAbundantNumbers',  () => {
+    describe('for [12] and [12,24,36,37]', ()=>{
+      it('should return [37]',  () => {
+        expect(filterOutMultiplesOfAbundantNumbers([12], [12, 24, 36, 37])).toEqual([37]);
+      });
+    });
+    describe('for [12, 20] and [12, 36, 40, 80, 37]', ()=>{
+      it('should return [37]',  () => {
+        expect(filterOutMultiplesOfAbundantNumbers([12, 20], [12, 36, 37, 40, 80])).toEqual([37]);
+      });
+    });
+  });
+
+  describe('filterOutSumsOfAbundantNumbers',  () => {
+    describe('for [12,20] and [32, 37, 64]', ()=>{
+      it('should return [37, 64]',  () => {
+        expect(filterOutSumsOfAbundantNumbers([12, 20], [32, 37, 64])).toEqual([37, 64]);
+      });
+    });
+  });
+
+  describe('sumNumbers',  () => {
+    describe('for [1,2,3]', ()=>{
+      it('should return 6',  () => {
+        expect(sumNumbers([1,2,3])).toEqual(6);
+      });
+    });
+  });
+
+  describe('findSum',  () => {
+    it('should return ???',  () => {
+      expect(findSum()).toEqual(4179871);
+    });
+  });
+
 })();
