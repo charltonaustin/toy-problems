247c8747c0a6a823b17aa9584ac292fe53fa6603
charltonaustin <charlton.austin@gmail.com>
2016-08-07 13:05:43 +0300

    Adding in the solution to the toy problem today.

--- euler-problem-024/src/problem.js
+++ euler-problem-024/src/problem.js
@@ -1,5 +1,82 @@
-var someFunction = () => {
-  return 0;
+/*
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+*/
+
+var findLargestPosition = (array) => {
+// Function to find the largest position
+// where elements are out of order
+  var position = -1;
+  for(var i = 0; i < array.length - 1; i++){
+    if(array[i] < array[i+1]){
+      position = i;
+    }
+  }
+  return position;
 }
 
-module.exports = {someFunction};
+var findSmallestPositionToRight = (array, position) => {
+// Function to find the position of the smallest element larger than
+// the element at the position
+  var currentElement = array[position];
+  var returnPosition = -1;
+  var returnElement = Number.POSITIVE_INFINITY;
+  for(var i = position+1; i < array.length; i++){
+    if(array[i] > currentElement && array[i] < returnElement){
+      returnElement = array[i];
+      returnPosition = i;
+    }
+  }
+  return returnPosition;
+}
+
+var swap = (firstPosition, secondPosition, array) => {
+  var temp = array[firstPosition];
+  array[firstPosition] = array[secondPosition];
+  array[secondPosition] = temp;
+  return array;
+}
+
+var reOrderRemaining = (array, position) => {
+// A function to reorder all elements
+// to the right of the position
+  var toRight = array.slice(position+1);
+  toRight.sort();
+  var reOrderedArray = [];
+  for(var i = 0; i < array.length; i++){
+    if(i <= position){
+      reOrderedArray.push(array[i]);
+    }else{
+      reOrderedArray.push(toRight[i-position-1]);
+    }
+  }
+  return reOrderedArray;
+}
+
+var findNextPermutation = (array) => {
+  var largestPosition = findLargestPosition(array);
+  var smallestPositionRight = findSmallestPositionToRight(array, largestPosition);
+  array = swap(largestPosition, smallestPositionRight, array);
+  array = reOrderRemaining(array, largestPosition);
+  return array;
+}
+
+var findPermutation = (array, number) => {
+  var permutation = array.slice(0);
+
+  for(var i = 1; i < number; i++){
+    permutation = findNextPermutation(permutation);
+  }
+
+  return permutation;
+}
+
+module.exports = {findNextPermutation,
+                  findLargestPosition,
+                  findSmallestPositionToRight,
+                  swap,
+                  reOrderRemaining,
+                  findPermutation};
--- euler-problem-024/test/mySpec.js
+++ euler-problem-024/test/mySpec.js
@@ -1,10 +1,59 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+
+  var swap = problem.swap;
+  describe('swap',  () => {
+    it('should switch two elements',  () => {
+      expect(swap(0, 1, [1, 2])).toEqual([2, 1]);
     });
   });
+
+  var findLargestPosition = problem.findLargestPosition;
+  describe('findLargestPosition',  () => {
+    it('should find i such that a_i < a_i+1',  () => {
+      expect(findLargestPosition([3,1,2])).toEqual(1);
+    });
+    it('should return -1 if elements are in order',  () => {
+      expect(findLargestPosition([3,2,1])).toEqual(-1);
+    });
+  });
+
+  var findSmallestPositionToRight = problem.findSmallestPositionToRight;
+  describe('findSmallestPositionToRight',  () => {
+    it('should find i such that a_i is less than everything to the right and greater than a_position',  () => {
+      expect(findSmallestPositionToRight([3, 1, 4, 3, 2, 6, 9], 1)).toEqual(4);
+    });
+  });
+
+  var reOrderRemaining = problem.reOrderRemaining;
+  describe('reOrderRemaining',  () => {
+    it('should reoder everything to the right',  () => {
+      expect(reOrderRemaining([3, 2, 4, 3, 1, 6, 9], 1)).toEqual([3, 2, 1, 3, 4, 6, 9]);
+    });
+  });
+
+  var findNextPermutation = problem.findNextPermutation;
+  describe('findNextPermutation',  () => {
+    it('should return the next permutation',  () => {
+      expect(findNextPermutation([0, 1, 2])).toEqual([0, 2, 1]);
+      expect(findNextPermutation([0, 2, 1])).toEqual([1, 0, 2]);
+      expect(findNextPermutation([1, 0, 2])).toEqual([1, 2, 0]);
+      expect(findNextPermutation([1, 2, 0])).toEqual([2, 0, 1]);
+      expect(findNextPermutation([2, 0, 1])).toEqual([2, 1, 0]);
+      expect(findNextPermutation([2, 1, 0])).toEqual([0, 1, 2]);
+    });
+  });
+
+  var findPermutation = problem.findPermutation;
+  describe('findPermutation',  () => {
+    it('should return the specified permutation',  () => {
+      expect(findPermutation([0, 1, 2], 1)).toEqual([0, 1, 2]);
+      expect(findPermutation([0, 1, 2], 6)).toEqual([2, 1, 0]);
+    });
+    it('should return millionth permutation',  () => {
+      expect(findPermutation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1000000)).toEqual([ 2, 7, 8, 3, 9, 1, 5, 4, 6, 0]);
+    })
+  });
+
 })();
