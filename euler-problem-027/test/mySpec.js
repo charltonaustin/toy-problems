5d4b129d21ba63b1305bfd7097ca332b5dc56fed
charltonaustin <charlton.austin@gmail.com>
2016-08-11 09:18:41 +0300

    Finding the lower bound for the number of primes that need to be generated.

--- euler-problem-027/test/mySpec.js
+++ euler-problem-027/test/mySpec.js
@@ -4,7 +4,7 @@
   var maxNumberOfPrimes = problem.maxNumberOfPrimes;
   describe('maxNumberOfPrimes',  () => {
     it('should return -59231',  () => {
-      expect(maxNumberOfPrimes(1250)).toBe(-59231);
+      expect(maxNumberOfPrimes(1230)).toBe(-59231);
     });
   });
 })();
