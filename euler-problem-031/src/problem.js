572fc19873b4d376e58e2ea158a884335d17b8b8
charltonaustin <charlton.austin@gmail.com>
2016-08-14 17:02:39 +0300

    Adding in the solution to today's toy problem. I had help though from http://www.algorithmist.com/index.php/Coin_Change :(

--- euler-problem-031/src/problem.js
+++ euler-problem-031/src/problem.js
@@ -1,5 +1,38 @@
-var someFunction = () => {
-  return 0;
+/*
+In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+
+1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+It is possible to make £2 in the following way:
+
+1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+How many different ways can £2 be made using any number of coins?
+*/
+
+var alreadyCalculated = {};
+var count = (denominations, index, amount) => {
+  if(alreadyCalculated[index+":"+amount]){
+    return alreadyCalculated[index+":"+amount];
+  }
+  if(amount === 0){
+    return 1;
+  }
+
+  if(amount < 0){
+    return 0;
+  }
+
+  if(index <= 0 && amount > 0){
+    return 0;
+  }
+  var returnAmount = count(denominations, index - 1, amount) + count(denominations, index, amount-denominations[index-1]);
+  alreadyCalculated[index+":"+amount] = returnAmount;
+  return returnAmount
+} 
+
+var differentWays = (amount) => {
+  debugger;
+  var denominations = [1, 2, 5, 10, 20, 50, 100, 200];
+  return count(denominations, denominations.length, amount);
 }
 
-module.exports = {someFunction};
+module.exports = {differentWays};
--- euler-problem-031/test/mySpec.js
+++ euler-problem-031/test/mySpec.js
@@ -1,10 +1,19 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var differentWays = problem.differentWays;
+  describe('differentWays',  () => {
+    it('should return 1 for 1p',  () => {
+      expect(differentWays(1)).toBe(1);
+    });
+    it('should return 2 for 2p',  () => {
+      expect(differentWays(2)).toBe(2);
+    });
+    it('should return 3 for 4p',  () => {
+      expect(differentWays(4)).toBe(3);
+    });
+    it('should return 3 for 200p',  () => {
+      expect(differentWays(200)).toBe(73682);
     });
   });
 })();
