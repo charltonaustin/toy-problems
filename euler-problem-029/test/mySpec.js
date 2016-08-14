b5952c0b88a98b061d9db8e91068425ce945dc92
charltonaustin <charlton.austin@gmail.com>
2016-08-14 11:01:44 +0300

    Adding in the solution to euler-problem-029 trying to catch up a bit.

--- euler-problem-029/test/mySpec.js
+++ euler-problem-029/test/mySpec.js
@@ -1,10 +1,13 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var distinctNumbers = problem.distinctNumbers;
+  describe('distinctNumbers',  () => {
+    it('should return 15 for 5',  () => {
+      expect(distinctNumbers(5)).toBe(15);
+    });
+    it('should return 15 for 100',  () => {
+      expect(distinctNumbers(100)).toBe(9183);
     });
   });
 })();
