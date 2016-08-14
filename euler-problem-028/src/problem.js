e836764e4aed25c848f1f029696006be3fb26645
charltonaustin <charlton.austin@gmail.com>
2016-08-14 10:34:06 +0300

    Adding in the solution to euler problem 28 because I'm a bit behind.

--- euler-problem-028/src/problem.js
+++ euler-problem-028/src/problem.js
@@ -1,5 +1,101 @@
-var someFunction = () => {
-  return 0;
+/*
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+*/
+
+var sumDiagonals = (matrix) => {
+  var sum = 0;
+  for(var i = 0; i < matrix[0].length; i++){
+    sum +=matrix[i][i]
+    if(i !== matrix[0].length - 1 - i){
+      sum += matrix[matrix[0].length - 1 - i][i]
+    }
+  }
+  return sum;
 }
 
-module.exports = {someFunction};
+var createMatrix = (numberOfEdges, startingPosition, size) => {
+  var matrix = [];
+  for(var i = 0; i <= numberOfEdges; i++){
+    var currentRow = [];
+    for(var j = 0; j <= numberOfEdges; j++){
+      currentRow.push(0);
+    }
+    matrix.push(currentRow);
+  }
+  var i = startingPosition;
+  var j = startingPosition;
+  var numberOfElements = 1;
+  matrix[i][j] = numberOfElements;
+  var addI = 1;
+  var addJ = 1;
+  while(numberOfElements < size){
+    for(var k = 1; k <= addI; k++){
+      if(numberOfElements < size){
+        i += 1
+        numberOfElements++;
+        matrix[j][i] = numberOfElements;
+      }
+    }
+    for(var k = 1; k<= addJ; k++){
+      if(numberOfElements < size){
+        j += 1;
+        numberOfElements++;
+        matrix[j][i] = numberOfElements;
+      }
+    }
+    addI++
+    addJ++
+    for(var k = 1; k <= addI; k++){
+      if(numberOfElements < size){
+        i -= 1
+        numberOfElements++;
+        matrix[j][i] = numberOfElements;
+      }
+    }
+    for(var k = 1; k<= addJ; k++){
+      if(numberOfElements < size){
+        j -= 1;
+        numberOfElements++;
+        matrix[j][i] = numberOfElements;
+      }
+    }
+    addI++
+    addJ++
+  }
+  return matrix;
+}
+
+var countElements = (numberOfEdges) => {
+  var currentNumberOfEdges = 0;
+  var currentSize = 1;
+  var nextOdd = 3;
+  while(currentNumberOfEdges < numberOfEdges){
+    currentNumberOfEdges++
+    currentSize += nextOdd
+    nextOdd+=2;
+  }
+  return currentSize;
+}
+
+var findStartingPosition = (numberOfEdges) => {
+  return Math.floor(numberOfEdges/2);
+}
+
+var findSum = (sprialSize) => {
+  var numberOfEdges =  sprialSize - 1;
+  var numberOfElements = countElements(numberOfEdges);
+  var startingPosition = findStartingPosition(numberOfEdges);
+  var matrix = createMatrix(numberOfEdges, startingPosition, numberOfElements)
+  return sumDiagonals(matrix);
+}
+module.exports = {countElements, createMatrix, sumDiagonals, findSum};
--- euler-problem-028/test/mySpec.js
+++ euler-problem-028/test/mySpec.js
@@ -1,10 +1,71 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var countElements = problem.countElements;
+  describe('countElements',  () => {
+    it('should return 0 for size 1',  () => {
+      expect(countElements(0)).toBe(1);
+    });
+    it('should return 1 for size 4',  () => {
+      expect(countElements(1)).toBe(4);
+    });
+    it('should return 2 for size 9',  () => {
+      expect(countElements(2)).toBe(9);
+    });
+    it('should return 3 for size 16',  () => {
+      expect(countElements(3)).toBe(16);
+    });
+    it('should return 4 for size 25',  () => {
+      expect(countElements(4)).toBe(25);
+    });
+    it('should return 5 for size 36',  () => {
+      expect(countElements(5)).toBe(36);
+    });
+    it('should return 6 for size 49',  () => {
+      expect(countElements(6)).toBe(49);
+    });
+  });
+  var createMatrix = problem.createMatrix;
+  describe('createMatrix',  () => {
+    it('should create a 2 by 2 matrix',  () => {
+      expect(JSON.stringify(createMatrix(1, 0, 4))).toEqual(JSON.stringify([[1, 2],[4,3]]));
+    });
+  });
+  describe('createMatrix',  () => {
+    it('should create a 3 by 3',  () => {
+      expect(JSON.stringify(createMatrix(2, 1, 9))).toEqual(JSON.stringify([[7,8,9],[6,1,2],[5, 4, 3]]));
+    });
+  });
+  describe('createMatrix',  () => {
+    it('should create a 4 by 4',  () => {
+      expect(JSON.stringify(createMatrix(3, 1, 16))).toEqual(JSON.stringify([[7,8,9,10],[6,1,2,11],[5, 4, 3,12],[16,15,14,13]]));
+    });
+  });
+  var sumDiagonals = problem.sumDiagonals;
+  describe('sumDiagonals',  () => {
+    it('should sum a 2 by 2 matrix',  () => {
+      expect(sumDiagonals([[1,2],[4,3]])).toEqual(10);
+    });
+    it('should sum a 3 by 3 matrix',  () => {
+      expect(sumDiagonals([[7,8,9],[6,1,2],[5,4,3]])).toEqual(25);
+    });
+    it('should sum a 5 by 5 matrix',  () => {
+      expect(sumDiagonals([[21,22,23,24,25],[20,7,8,9,10],[19,6,1,2,11],[18,5,4,3,12],[17,16,15,14,13]])).toEqual(101);
+    });
+  });
+  var findSum = problem.findSum;
+  describe('findSum',  () => {
+    it('should sum a 5 by 5',  () => {
+      expect(findSum(5)).toEqual(101);
+    });
+    it('should sum a 2 by 2',  () => {
+      expect(findSum(2)).toEqual(10);
+    });
+    it('should sum a 3 by 3',  () => {
+      expect(findSum(3)).toEqual(25);
+    });
+    it('should sum a 1001 by 1001',  () => {
+      expect(findSum(1001)).toEqual(669171001);
     });
   });
 })();
