5b557b82cf7f1085382cb7fa65ffb07976b196a0
charltonaustin <charlton.austin@gmail.com>
2016-08-16 11:08:01 +0300

    Adding in the solution to today's problem.

--- euler-problem-033/src/problem.js
+++ euler-problem-033/src/problem.js
@@ -1,5 +1,36 @@
-var someFunction = () => {
-  return 0;
+/*
+The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8,
+which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and
+denominator.
+
+If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+*/
+
+var findDen = () => {
+  var num = 1;
+  var den = 1;
+  for(var i = 11; i < 100; i++){
+    for(var j = i+1; j < 100; j++){
+      var topFirst = Math.floor(i/10);
+      var topSecond = i % 10;
+      var bottomFirst = Math.floor(j/10);
+      var bottomSecond = j % 10;
+      var fraction = i / j;
+      if(topSecond === bottomFirst){
+        if(fraction === topFirst/bottomSecond && bottomSecond !== 0){
+          num = num * i;
+          den = den * j;
+        }
+      }
+    }
+  }
+  return den/num;
 }
 
-module.exports = {someFunction};
+module.exports = {findDen};
+
+findDen()
--- euler-problem-033/test/mySpec.js
+++ euler-problem-033/test/mySpec.js
@@ -1,10 +1,10 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
+  var findDen = problem.findDen;
+  describe('findDen',  () => {
     it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+      expect(findDen()).toBe(100);
     });
   });
 })();
