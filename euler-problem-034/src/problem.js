8f307139e36dffce8d3a4cc65e7e7cd5c2966909
charltonaustin <charlton.austin@gmail.com>
2016-08-17 11:17:50 +0300

    Adding in the solution to today's toy problem.

--- euler-problem-034/src/problem.js
+++ euler-problem-034/src/problem.js
@@ -1,5 +1,24 @@
-var someFunction = () => {
-  return 0;
+var factorials = {0: 1, 1: 1, 2: 2, 3: 6, 4: 24, 5: 120, 6: 720, 7: 5040, 8: 40320, 9: 362880}
+
+var sumOfFactorialOfDigits = (number) => {
+  var total = 0;
+  while(number > 0){
+    var digit = number % 10;
+    number = Math.floor(number / 10);
+    total += factorials[digit];
+  }
+  return total;
 }
 
-module.exports = {someFunction};
+var digitFactorials = (limit) => {
+  var total = 0;
+  for(var n = 10; n < limit; n++){
+    var sum = sumOfFactorialOfDigits(n);
+    if(sum === n){
+      total+= n;
+    }
+  }
+  return total;
+}
+
+module.exports = {sumOfFactorialOfDigits, digitFactorials};
--- euler-problem-034/test/mySpec.js
+++ euler-problem-034/test/mySpec.js
@@ -1,10 +1,21 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var sumOfFactorialOfDigits= problem.sumOfFactorialOfDigits;
+  describe('sumOfFactorialOfDigits',  () => {
+    it('should return 4',  () => {
+      expect(sumOfFactorialOfDigits(120)).toBe(4);
+      expect(sumOfFactorialOfDigits(1000)).toBe(4);
+    });
+  });
+
+  var digitFactorials = problem.digitFactorials;
+  describe('digitFactorials',  () => {
+    it('should return 4',  () => {
+      expect(digitFactorials(146)).toBe(145);
+    });
+    it('should return the sum of all digitFactorials',  () => {
+      expect(digitFactorials(10000000)).toBe(40730);
     });
   });
 })();
