3d55b5eed6f0b1a1734156243315d83671a6f806
charltonaustin <charlton.austin@gmail.com>
2016-08-18 07:44:39 +0300

    Adding in the solution to today's toy problem.

--- euler-problem-035/src/problem.js
+++ euler-problem-035/src/problem.js
@@ -1,5 +1,81 @@
-var someFunction = () => {
-  return 0;
+/*
+The number, 197, is called a circular prime
+because all rotations of the digits: 197,
+971, and 719, are themselves prime.
+
+There are thirteen such primes below 100:
+2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73,
+79, and 97.
+
+How many circular primes are there below
+one million?
+*/
+
+var createPrimeSet = (primes) => {
+  var primeSet = {}
+  for(var i = 0; i < primes.length; i++){
+    primeSet[primes[i]] = true;
+  }
+  return primeSet;
 }
 
-module.exports = {someFunction};
+var findPrimeN = function(number){
+  var primes = [2, 3]
+  var numbers = [];
+  while(primes[primes.length - 1] < number){
+    // Grow the numbers array
+    var start = primes[primes.length-1] + 2;
+    var growth = primes[primes.length-1];
+    for(var i = start; i < start + growth; i+=2){
+      numbers.push(i);
+    }
+    // Remove composite numbers
+    for(var i = 0; i < primes.length; i++){
+      for(var j = 0; j < numbers.length; j++){
+        if(numbers[j] % primes[i] === 0){
+          numbers.splice(j,1);
+        }
+      }
+    }
+    // move prime numbers to primes
+    for(var i = 0; i < numbers.length; i++){
+      primes.push(numbers[i]);
+    }
+    numbers = [];
+  }
+  return primes;
+}
+
+var rotateNumber = (number) => {
+  if(number < 10){
+    return number;
+  }
+  var digit = number % 10;
+  if(digit === 0){
+    return number;
+  }
+  var base = Math.floor(number / 10);
+  return parseInt(digit+""+base);
+}
+
+var findNumberOfCircularPrimes = (limit) => {
+  var primeSet = createPrimeSet(findPrimeN(limit));
+  var circularPrimes = [];
+  for(var prime in primeSet){
+    var prime = parseInt(prime);
+    var r = rotateNumber(prime);
+    var allArePrime = true;
+    while(prime !== r && allArePrime){
+      if(!primeSet[r]){
+        allArePrime = false;
+      }
+      r = rotateNumber(r);
+    }
+    if(allArePrime && prime < limit){
+      circularPrimes.push(prime);
+    }
+  }
+  return circularPrimes.length;
+}
+
+module.exports = {rotateNumber, findNumberOfCircularPrimes};
--- euler-problem-035/test/mySpec.js
+++ euler-problem-035/test/mySpec.js
@@ -1,10 +1,28 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var rotateNumber = problem.rotateNumber;
+  describe('rotateNumber',  () => {
+    it('should return same number for single digits',  () => {
+      expect(rotateNumber(3)).toBe(3);
+    });
+    it('should return same number if digit is zero',  () => {
+      expect(rotateNumber(170)).toBe(170);
+    });
+    it('should return same number after multiple calls',  () => {
+      expect(rotateNumber(171)).toBe(117);
+      expect(rotateNumber(117)).toBe(711);
+      expect(rotateNumber(711)).toBe(171);
+    });
+  });
+
+  var findNumberOfCircularPrimes = problem.findNumberOfCircularPrimes;
+  describe('findNumberOfCircularPrimes',  () => {
+    it('should return 13 for less than 100',  () => {
+      expect(findNumberOfCircularPrimes(100)).toBe(13);
+    });
+    it('should return 13 for less than 1 million',  () => {
+      expect(findNumberOfCircularPrimes(1000000)).toBe(55);
     });
   });
 })();
