3447cf51a531efc742e875fcd0a18435f1fb0d39
charltonaustin <charlton.austin@gmail.com>
2016-08-21 18:19:40 +0300

    Adding in the solution to today's toy problem

--- euler-problem-037/src/problem.js
+++ euler-problem-037/src/problem.js
@@ -1,5 +1,71 @@
-var someFunction = () => {
-  return 0;
+/*
+The number 3797 has an interesting property. Being prime itself, it is possible to
+continuously remove digits from left to right, and remain prime at each stage:
+3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37,
+and 3.
+
+Find the sum of the only eleven primes that are both truncatable from left
+to right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+*/
+var alreadyFound = {};
+var isPrime = (number) => {
+  if(alreadyFound[number]){
+    return alreadyFound[number];
+  }
+  for(var i = 2; i < number; i++){
+    if(number % i === 0){
+      alreadyFound[number] = false;
+      return false;
+    }
+  }
+  alreadyFound[number] = true;
+  return true;
 }
 
-module.exports = {someFunction};
+var checkTruncateLeft = (number) =>{
+  for(var i = 2; i < number.length; i++){
+    var temp = parseInt(number.slice(0,i))
+    if(!isPrime(temp)){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+var checkTruncateRight = (number) => {
+  for(var i = 0; i < number.length; i++){
+    var temp = parseInt(number.substring(number.length,i))
+    if(!isPrime(temp)){
+      return false;
+    }
+  }
+  return true;
+}
+
+var findTruncatablePrimes = () => {
+  var truncatablePrimes = [23, 37, 53, 73]
+  var k = 0;
+  var threeSeven = [3,7];
+  while(truncatablePrimes.length < 11){
+    var number = "" + k
+    for(var i = 0; i < threeSeven.length; i++){
+      for(var j = 0; j < threeSeven.length; j++){
+        var possibleTruncatablePrime = threeSeven[i] + number + threeSeven[j];
+        if(checkTruncateLeft(possibleTruncatablePrime) && checkTruncateRight(possibleTruncatablePrime)){
+          truncatablePrimes.push(parseInt(possibleTruncatablePrime))
+        }
+      }
+    }
+    k++
+  }
+  var total = 0;
+  for(var i = 0; i < truncatablePrimes.length; i++){
+    total+= truncatablePrimes[i];
+  }
+  return total;
+}
+
+module.exports = {findTruncatablePrimes, checkTruncateLeft, checkTruncateRight};
--- euler-problem-037/test/mySpec.js
+++ euler-problem-037/test/mySpec.js
@@ -1,10 +1,10 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
+  var findTruncatablePrimes = problem.findTruncatablePrimes;
+  describe('findTruncatablePrimes',  () => {
     it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+      expect(findTruncatablePrimes()).toBe(748317);
     });
   });
 })();
