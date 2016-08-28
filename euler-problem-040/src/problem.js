91c8d94069ddf3bd84c4fbf4a931c770ab2b8362
charltonaustin <charlton.austin@gmail.com>
2016-08-28 10:02:50 +0300

    Adding in the solution to today's toy problem

--- euler-problem-040/src/problem.js
+++ euler-problem-040/src/problem.js
@@ -1,5 +1,30 @@
-var someFunction = () => {
-  return 0;
+/*
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.123456789101112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If d_n represents the nth digit of the fractional part, find the value of the following expression.
+
+d_1 × d_10 × d_100 × d_1000 × d_10000 × d_100000 × d_1000000
+*/
+
+var findNthSpot = (number, spot) => {
+  return parseInt(number.charAt(spot - 1))
 }
 
-module.exports = {someFunction};
+var findExpression = (limit) => {
+  var irrational = '';
+  var limit = Math.pow(10, limit-1)
+  for(var i = 1; i <= limit; i++){
+    irrational+=i
+  }
+  var result = 1;
+  for(var i = 1; i <= limit; i*=10){
+    result*= findNthSpot(irrational, i);
+  }
+  return result;
+}
+
+module.exports = {findExpression};
--- euler-problem-040/test/mySpec.js
+++ euler-problem-040/test/mySpec.js
@@ -1,10 +1,19 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
+  var findExpression = problem.findExpression;
+  describe('findExpression',  () => {
     it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+      //expect(findExpression(1)).toBe(1);
+    });
+    it('should return 2',  () => {
+      //expect(findExpression(2)).toBe(1);
+    });
+    it('should return 6',  () => {
+      expect(findExpression(3)).toBe(5);
+    });
+    it('should return ??? for 6',  () => {
+      expect(findExpression(7)).toBe(210);
     });
   });
 })();
