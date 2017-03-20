6c5ed82d9dc2b7c1c3cca446af7145e2c1bc3f86
Charlton Austin <charlton.austin@gmail.com>
2017-03-19 22:04:09 -0400

    Adding in some basic functions for Euler 42

--- euler-problem-042/src/problem.js
+++ euler-problem-042/src/problem.js
@@ -1,5 +1,26 @@
+fs = require('fs')
+
+var readInFile = (fileReader=fs) => {
+    return fileReader.readFileSync('../words.txt');
+}
+
+var splitWords = (words) => {
+    return words.split(",");
+}
+
+var convertToNumber = (letter) => {
+    if (letter.length == 1){
+	return letter.toLowerCase().charCodeAt(0) - 96;
+    }
+}
+
 var someFunction = () => {
   return 0;
 }
 
-module.exports = {someFunction};
+
+module.exports = {someFunction,
+		  readInFile,
+		  splitWords,
+		  convertToNumber,
+		 };
--- euler-problem-042/test/mySpec.js
+++ euler-problem-042/test/mySpec.js
@@ -1,10 +1,28 @@
 (function () {
   'use strict';
   var problem = require('../src/problem.js');
-  var someFunction = problem.someFunction;
-  describe('someFunction',  () => {
-    it('should return 1',  () => {
-      expect(someFunction()).toBe(1);
+  var readInFile = problem.readInFile;
+  describe('readInFile',  () => {
+    it('check that readInFile returns correct string',  () => {
+	var fileReader = {readFileSync: ()=>{return "File contents"}};
+	expect(readInFile(fileReader)).toBe("File contents");
+    });
+  });
+
+  var splitWords = problem.splitWords;
+  describe('splitWords',  () => {
+    it('check that words are split correctly',  () => {
+	expect(splitWords("a,b,c")).toEqual(["a","b","c"]);
+    });
+  });
+
+  var convertToNumber = problem.convertToNumber;
+  describe('convertToNumber',  () => {
+    it('check that values get converted correctly',  () => {
+	expect(convertToNumber("a")).toBe(1);
+	expect(convertToNumber("A")).toBe(1);
+	expect(convertToNumber("z")).toBe(26);
+	expect(convertToNumber("Z")).toBe(26);
     });
   });
 })();
