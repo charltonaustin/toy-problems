3e36fe0e431a5d9a8141bde778aa0a81ac6df044
charltonaustin <charlton.austin@gmail.com>
2022-07-21 13:32:41 -0400

    Coding the solution and the tests

--- euler-problem-043/src/index.test.ts
+++ euler-problem-043/src/index.test.ts
@@ -1,10 +1,72 @@
-const {eulerProblem043, generateZeroToNine} = require('./index');
+const {
+    eulerProblem043,
+    generatePermutations,
+    swap,
+    generateSubNumbers,
+    checkForProperty,
+    getNumber,
+    sumNumbers,
+} = require('./index');
+export {}
 
 test('euler problem gives back the sum of all pandigital numbers', () => {
-    expect(eulerProblem043()).toBe(0);
+    expect(eulerProblem043()).toBe(16695334890);
 });
 
-test('generatePandigitalNumbers returns and array of all pandigital numbers', () => {
-    expect(generateZeroToNine()).toEqual([]);
+test('generatePermutation returns a nested array for an empty array', () => {
+    expect(generatePermutations([])).toEqual([[]]);
 });
-export {}
+
+test('generatePermutation returns a nested single entry for a single value', () => {
+    expect(generatePermutations(["a"])).toEqual([["a"]]);
+});
+
+test('generatePermutation returns swapped arrays for two values', () => {
+    expect(generatePermutations(["a", "b"])).toEqual([["a", "b"], ["b", "a"]]);
+});
+
+test('generatePermuation returns the correct length of array', () => {
+    expect(generatePermutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).length).toEqual(3628800)
+})
+
+test('swap returns swaps two values', () => {
+    expect(swap(["a", "b"], 0, 1)).toEqual(["b", "a"]);
+});
+
+test('swap returns values if j is too large', () => {
+    expect(swap(["a"], 0, 1)).toEqual(["a"]);
+});
+
+test('swap returns values if i is too large', () => {
+    expect(swap(["a"], 1, 0)).toEqual(["a"]);
+});
+
+test('generateSubNumbers returns first correct sub number', () => {
+    expect(generateSubNumbers([1, 4, 0, 6, 3, 5, 7, 2, 8, 9])[0]).toEqual(406);
+});
+
+test('generateSubNumbers returns second correct sub number', () => {
+    expect(generateSubNumbers([1, 4, 0, 6, 3, 5, 7, 2, 8, 9])[1]).toEqual(63);
+});
+
+test('generateSubNumbers returns last correct sub number', () => {
+    expect(generateSubNumbers([1, 4, 0, 6, 3, 5, 7, 2, 8, 9])[6]).toEqual(289);
+});
+
+test('checkForProperty returns true for 1406357289', () => {
+    const values = [406, 63, 635, 357, 572, 728, 289]
+    expect(checkForProperty(values)).toEqual(true);
+});
+
+test('checkForProperty returns false for 1063457289', () => {
+    const values = [63, 634, 345, 457, 572, 728, 289]
+    expect(checkForProperty(values)).toEqual(false);
+});
+
+test('getNumber returns false for 1063457289', () => {
+    expect(getNumber([1, 4, 0, 6, 3, 5, 7, 2, 8, 9])).toEqual(1406357289);
+});
+
+test('sumNumbers returns false for 1063457289', () => {
+    expect(sumNumbers([1, 1, 1])).toEqual(3);
+});
--- euler-problem-043/src/index.ts
+++ euler-problem-043/src/index.ts
@@ -1,12 +1,123 @@
-const eulerProblem043 = () => {
-    return 0
+function eulerProblem043(): number {
+    const permutations = generatePermutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    const sumArray: number[] = []
+    for (let i = 0; i < permutations.length; i++) {
+        const subNumbers = generateSubNumbers(permutations[i])
+        if (checkForProperty(subNumbers)) {
+            sumArray.push(getNumber(permutations[i]))
+        }
+    }
+    return sumNumbers(sumArray)
 }
 
-const generateZeroToNine: () => Number[] = () => {
-    return []
+function getNumber(permutation: number[]) {
+    let numberString = ""
+
+    for (let j = 0; j < permutation.length; j++) {
+        if (j == 0 && permutation[j] == 0) {
+            // do nothing
+        } else {
+            numberString += Number(permutation[j]).toString()
+        }
+    }
+    return Number(numberString).valueOf();
+}
+
+function sumNumbers(sumArray: number[]) {
+    let sum = 0
+    for (let i = 0; i < sumArray.length; i++) {
+        sum += sumArray[i]
+    }
+    return sum;
+}
+
+function checkForProperty(pandigitalSubNumbers: number[]) {
+    if (pandigitalSubNumbers.length != 7) {
+        return false
+    }
+    const primes = [2, 3, 5, 7, 11, 13, 17]
+    for (let i = 0; i < pandigitalSubNumbers.length; i++) {
+        if (pandigitalSubNumbers[i] % primes[i] != 0) {
+            return false
+        }
+    }
+    return true
+}
+
+function generateSubNumbers(values: number[]) {
+    if (values.length != 10) {
+        throw "length of values array should be 10"
+    }
+    const subNumbers: number[] = []
+    for (let i = 1; i < 8; i++) {
+        const iString = Number(values[i]).toString()
+        const jString = Number(values[i + 1]).toString()
+        const kString = Number(values[i + 2]).toString()
+        if (iString == "0") {
+            subNumbers.push(Number(jString + kString).valueOf())
+        } else {
+            subNumbers.push(Number(iString + jString + kString))
+        }
+    }
+    return subNumbers
+}
+
+function swap<T>(array: T[], i: number, j: number): T[] {
+    if (i >= array.length || j >= array.length) {
+        return array
+    }
+
+    const newArray: T[] = []
+    for (let k = 0; k < array.length; k++) {
+        if (k == i) {
+            newArray.push(array[j])
+        } else if (k == j) {
+            newArray.push(array[i])
+        } else {
+            newArray.push(array[k])
+        }
+    }
+    return newArray
+}
+
+function generatePermutations<T>(values: T[]): T[][] {
+    const returnArray: T[][] = []
+    const stack: number[] = []
+    for (let i = 0; i < values.length; i++) {
+        stack.push(0)
+    }
+    const newArray: T[] = []
+    for (let i = 0; i < values.length; i++) {
+        newArray.push(values[i])
+    }
+
+    returnArray.push(newArray)
+
+    let i = 1;
+    while (i < values.length) {
+        if (stack[i] < i) {
+            if (i % 2 == 0) {
+                values = swap(values, 0, i)
+            } else {
+                values = swap(values, stack[i], i)
+            }
+            returnArray.push(values)
+            stack[i] += 1
+            i = 1
+        } else {
+            stack[i] = 0
+            i += 1
+        }
+    }
+    return returnArray
 }
 
 module.exports = {
     eulerProblem043,
-    generateZeroToNine,
+    generatePermutations,
+    swap,
+    generateSubNumbers,
+    checkForProperty,
+    getNumber,
+    sumNumbers
 }
